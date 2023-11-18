374b279aba688fd8093e1f1f9a258ba150833382
Aban Qureshi <99500260+QuestyFella@users.noreply.github.com>
2023-11-18 12:02:23 +0530

    first line added to base api

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const cheerio = require('cheerio')
 
 const app = express()
 
+app.get('/',(req,res)=>{
+    res.json("Welcome to my Climate Change News API")
+})
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
