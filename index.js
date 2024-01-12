9727f95058be46df98cd5c2d50c913465c55ee85
Aban Qureshi <99500260+QuestyFella@users.noreply.github.com>
2024-01-12 12:30:06 +0530

    reverted shctuf

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const cheerio = require("cheerio");
 
 const app = express();
 
+const newspapers = [
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment/climate-change",
+    base: "",
+  },
+  {
+    name: "theguardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+    base: "",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/climate-change",
+    base: "",
+  },
+];
 const articles = [];
 
 app.get("/", (req, res) => {
